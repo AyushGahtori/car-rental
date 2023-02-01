5193baccbbafa7e762ea99cf461a461519b373ff
unknown <lol.delta123@gmail.com>
2023-02-01 13:28:33 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import BgShape from "../images/hero/hero-bg.png";
+import HeroCar from "../images/hero/main-car.png";
 
 function Hero() {
   return (
@@ -8,8 +10,30 @@ function Hero() {
           <img className="bg-shape" src={BgShape} alt="bg-shape" />
           <div className="hero-content">
             <div className="hero-content__text">
-              <p>suuuuui</p>
+              <h4>Plan your trip now</h4>
+              <h1>
+                Save <span>big</span> with our car rental
+              </h1>
+              <p>
+                Rent the car of your dreams. Unbeatable prices, unlimited miles,
+                flexible pick-up options and much more.
+              </p>
+              <div className="hero-content__text__btns">
+                <Link className="hero-content__text__btns__book-ride" to="/">
+                  Book Ride &nbsp; <i className="fa-solid fa-circle-check"></i>
+                </Link>
+                <Link className="hero-content__text__btns__learn-more" to="/">
+                  Learn More &nbsp; <i className="fa-solid fa-angle-right"></i>
+                </Link>
+              </div>
             </div>
+
+            {/* img */}
+            <img
+              src={HeroCar}
+              alt="car-img"
+              className="hero-content__car-img"
+            />
           </div>
         </div>
       </section>
