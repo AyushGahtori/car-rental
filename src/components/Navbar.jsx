868a0a0ecbc91196e6a42bb7e2921900eaec70f7
unknown <lol.delta123@gmail.com>
2023-02-02 18:45:18 +0100

    navbar mobile functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,46 @@
 import { Link } from "react-router-dom";
 import Logo from "../images/logo/logo.png";
+import { useState } from "react";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <>
       <nav>
+        {/* mobile */}
+        <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+          <div onClick={openNav} className="mobile-navbar__close">
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+          <ul className="mobile-navbar__links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/">Models</Link>
+            </li>
+            <li>
+              <Link to="/">Testimonials</Link>
+            </li>
+            <li>
+              <Link to="/">Our Team</Link>
+            </li>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* desktop */}
+
         <div className="navbar">
           <div className="navbar__img">
             <Link to="/" onClick={() => window.top(0, 0)}>
@@ -29,7 +65,7 @@ function Navbar() {
           </div>
 
           {/* mobile */}
-          <div className="mobile-hamb">
+          <div className="mobile-hamb" onClick={openNav}>
             <i className="fa-solid fa-bars"></i>
           </div>
         </div>
