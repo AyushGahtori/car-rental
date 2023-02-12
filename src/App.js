dc00824456d4ffb5dfa6066d974626d505f0e7e0
unknown <lol.delta123@gmail.com>
2023-02-12 12:54:22 +0100

    contact page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import Models from "./Pages/Models";
 import TestimonialsPage from "./Pages/TestimonialsPage";
 import Team from "./Pages/Team";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="models" element={<Models />} />
         <Route path="testimonials" element={<TestimonialsPage />} />
         <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </>
   );
